ef6f15767106bb7f8ed39a478d6b25193cc240c3
Fabio Pereira <fgpereira@gmail.com>
2020-04-22 20:39:44 -0300

    7.4-escopo-dos-beans-v1

--- src/main/java/com/algaworks/compras/Checkout.java
+++ src/main/java/com/algaworks/compras/Checkout.java
@@ -1,17 +1,24 @@
 package com.algaworks.compras;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(scopeName = "prototype")
 public class Checkout {
+	
+	private int count;
 	@Autowired
 	private Impressora impressora;
 
 	public void finalizar() {
 		// regras do checkout
 		impressora.imprimir("compras.csv");
+		count++;
 	}
 
-	
+	public int getQtdPedidosFinalizado() {
+	return count;	
+	}
 }
--- src/main/java/com/algaworks/compras/Main.java
+++ src/main/java/com/algaworks/compras/Main.java
@@ -11,8 +11,14 @@ public class Main {
 	public static void main(String[] args) {
 		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 		
-		Checkout checkout =context.getBean(Checkout.class);
-		checkout.finalizar();
+		Checkout c1 =context.getBean(Checkout.class);
+		c1.finalizar();
+		Checkout c2 =context.getBean(Checkout.class);
+		c2.finalizar();
+		Checkout c3 =context.getBean(Checkout.class);
+		c3.finalizar();
+		
+		System.out.println(c1.getQtdPedidosFinalizado());
 		
 		((ConfigurableApplicationContext) context).close();
 	}
