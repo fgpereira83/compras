4115cef22b5f3052113977c0f61e19cb781b8aa0
Fabio Pereira <fgpereira@gmail.com>
2020-04-22 20:17:18 -0300

    7.2-o-applicationcontext-v1

---
+++ src/main/java/compras/ImpressoraEpson.java
@@ -0,0 +1,8 @@
+package compras;
+
+public class ImpressoraEpson implements Impressora {
+
+	public void imprimir(String documento) {
+		System.out.println("imprimindo na espson: "+documento);
+	}
+}
---
+++ src/main/java/compras/Main.java
@@ -0,0 +1,17 @@
+package compras;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+		
+		Checkout checkout =context.getBean(Checkout.class);
+		checkout.finalizar();
+		
+		((ConfigurableApplicationContext) context).close();
+	}
+}
